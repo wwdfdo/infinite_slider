6210b0ad4b7a4bcef7f04ff8d1391281898796e9
wwdfdo <wwdfdo@gmail.com>
2022-05-27 11:24:17 +0530

    creating infinite slider

---
+++ src/components/arrays/featuredCollectionArray.js
@@ -0,0 +1,47 @@
+import shroom1 from "../images/shroom1.jpg";
+import shroom2 from "../images/shroom2.jpg";
+import shroom3 from "../images/shroom3.jpg";
+import shroom4 from "../images/shroom4.jpg";
+import shroom5 from "../images/shroom5.jpg";
+import shroom7 from "../images/shroom7.jpg";
+import shroom8 from "../images/shroom8.jpg";
+import shroom9 from "../images/shroom9.jpg";
+
+export const nums = [
+  {
+    id: 1,
+    img: shroom1,
+  },
+  {
+    id: 2,
+    img: shroom2,
+  },
+  {
+    id: 3,
+    img: shroom3,
+  },
+  {
+    id: 4,
+    img: shroom4,
+  },
+  {
+    id: 5,
+    img: shroom5,
+  },
+  {
+    id: 6,
+    img: shroom7,
+  },
+  {
+    id: 7,
+    img: shroom8,
+  },
+  {
+    id: 8,
+    img: shroom9,
+  },
+  {
+    id: 9,
+    img: shroom1,
+  },
+];
